29f5072abd0ad7f65741f1c90f6b1b4f0284183c
gaspare d'antone <dantogas@gmail.com>
2024-05-14 16:59:51 +0200

    integrazione

--- resources/views/aboutme.blade.php
+++ resources/views/aboutme.blade.php
@@ -7,13 +7,10 @@
                             <div class="my-5 text-center text-xl-start">
                                 <h1 class="display-5 fw-bolder text-white mb-2">Decennale esperienza al vostro servizio</h1>
                                 <p class="lead fw-normal text-white-50 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat harum suscipit ex eius odio eum magnam. Quibusdam dignissimos dolorum vitae nisi nulla laboriosam nesciunt reiciendis laudantium. Dicta ipsa debitis saepe!</p>
-                                {{-- <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                                    <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Get Started</a>
-                                    <a class="btn btn-outline-light btn-lg px-4" href="#!">Learn More</a>
-                                </div> --}}
+                                
                             </div>
                         </div>
-                        <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center"><img class="img-fluid rounded-3 my-5" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
+                       
                     </div>
                 </div>
             </header>
--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -9,19 +9,12 @@
                     </div>
                     <div class="row gx-5">
                         @foreach ($servizi as $element)
-                        <div class="col-lg-6">
-                            <div class="position-relative mb-5">
-                                <img class="img-fluid rounded-3 mb-3" src="{{$element['img']}}" alt="...">
-                                <a class="h3 fw-bolder text-decoration-none stretched-link text-white"
-                                href="{{ route('detail', ['service' => $element['uri']]) }}">
-                                {{ $element['name'] }}
-                            </a>  
-                            </div>
-                        </div>
-                            
+                            <x-card  :element="$element"   />
                         @endforeach
-                       
                     </div>
+                        
+                            
+                       
                 </div>
             </section>
 
